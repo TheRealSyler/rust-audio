adff314beb2bc23194a0adf00ab079e966128c9a
Leonard Grosoli <hileo77@gmail.com>
2021-07-11 16:50:16 +0200

    node test

--- js/src/index.ts
+++ js/src/index.ts
@@ -3,17 +3,14 @@ import rawData from './data.data';
 import { CanvasViewerRect } from './canvasViewerRect';
 import { map } from './utils';
 
-
-
 const data = rawData.split(',').map(v => parseFloat(v));
-
 
 const sampleRate = data.shift() || 0;
 
 let { min, max } = getMinMax(data);
 
-const viewLength = Infinity
-const skip = 10
+const viewLength = 2000
+const skip = 1
 const wave = data.slice(0, viewLength).map(v => map(v, min, max, -1, 1))
 
 const { min: minWave, max: maxWave } = getMinMax(wave)
@@ -47,4 +44,74 @@ function getMinMax(data: number[]) {
   return { min, max };
 }
 
+interface RefIn {
+  node?: Node<any, any>,
+  out?: string | number,
+  default: number,
+}
 
+interface Node<I extends string, O extends string> {
+  inputs: { [key in I]: RefIn },
+  outputs: { [key in O]: number },
+  compute: () => void,
+}
+
+const add: Node<'a' | 'b', never> = {
+  inputs: {
+    a: { default: 0 },
+    b: { default: 2 },
+  },
+  outputs: {},
+  compute: function () {
+    console.log('1', getRefVal(this.inputs.a) + getRefVal(this.inputs.b))
+  }
+}
+const add2: Node<'a' | 'b', 'res'> = {
+  inputs: {
+    a: { default: 1 },
+    b: { default: 1 },
+  },
+  outputs: {
+    res: 0,
+  },
+  compute: function () {
+    this.outputs.res = getRefVal(this.inputs.a) + getRefVal(this.inputs.b)
+    console.log('2', this.outputs.res);
+  }
+}
+
+add.inputs.a.node = add2
+add.inputs.a.out = 'res'
+compute(add)
+
+
+function compute(node: Node<any, any>) {
+  if (Array.isArray(node.inputs)) {
+    for (let i = 0; i < node.inputs.length; i++) {
+      const input = node.inputs[i];
+      if (input.node) {
+        compute(input.node)
+      }
+    }
+  } else {
+
+    for (const key in node.inputs) {
+      if (Object.prototype.hasOwnProperty.call(node.inputs, key)) {
+        const input = node.inputs[key];
+        if (input.node) {
+          compute(input.node)
+        }
+      }
+    }
+  }
+
+  node.compute()
+}
+
+
+function getRefVal(ref: RefIn): number {
+  if (ref.node && ref.out !== undefined) {
+    return ref.node.outputs[ref.out as never]
+  }
+  return ref.default
+}
--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,127 @@
-extern crate cpal;
+use std::collections::HashMap;
 
+use types::AudioValue;
+
+mod math_nodes;
+mod node;
+mod noise;
+mod types;
 mod waves;
 
-use std::fs::File;
-use std::io::prelude::*;
+struct NodeRef<'a> {
+  node: &'a NodeAdd<'a>,
+  output: &'a str,
+}
 
-use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+struct RefIn<'a> {
+  nodeRef: Option<NodeRef<'a>>,
+  default: AudioValue,
+}
 
-use crate::waves::wave;
+impl<'a> RefIn<'a> {
+  pub fn new() -> RefIn<'a> {
+    RefIn {
+      default: 0.0,
+      nodeRef: None,
+    }
+  }
+}
+
+trait Node<'a> {
+  fn outputs(&mut self) -> &Outputs<'a>;
+  fn inputs(&mut self) -> &Inputs<'a>;
+  fn compute(&mut self) -> ();
+}
+
+type Inputs<'a> = HashMap<&'a str, RefIn<'a>>;
+
+type Outputs<'a> = HashMap<&'a str, AudioValue>;
+
+struct NodeAdd<'a> {
+  _outputs: Outputs<'a>,
+  _inputs: Inputs<'a>,
+}
+
+impl<'a> NodeAdd<'a> {
+  pub fn new() -> NodeAdd<'a> {
+    let mut inputs: Inputs = HashMap::with_capacity(2);
+    inputs.insert("a", RefIn::new());
+    inputs.insert("b", RefIn::new());
+    let mut outputs: Outputs = HashMap::with_capacity(1);
+    outputs.insert("out", 0.0);
+    NodeAdd {
+      _inputs: inputs,
+      _outputs: outputs,
+    }
+  }
+}
+
+impl<'a> Node<'a> for NodeAdd<'a> {
+  fn inputs(&mut self) -> &Inputs<'a> {
+    &self._inputs
+  }
+  fn outputs(&mut self) -> &Outputs<'a> {
+    &self._outputs
+  }
+  fn compute(&mut self) -> () {
+    let val = get_input(self._inputs.get("a")) + get_input(self._inputs.get("b"));
+
+    println!("compute: {}", val);
+    self._outputs.insert("out", val);
+  }
+}
+
+fn get_input<'a>(ref_in: Option<&RefIn<'a>>) -> AudioValue {
+  let r = match ref_in {
+    Some(v) => v,
+    None => return 0.0,
+  };
+  println!("awd");
+  match &r.nodeRef {
+    Some(node) => match node.node._outputs.get(node.output) {
+      Some(val) => {
+        println!("awd2 ");
+        val.clone()
+      }
+      None => r.default,
+    },
+    None => r.default,
+  }
+}
 
 fn main() {
-  let host = cpal::default_host();
+  let mut n1 = NodeAdd::new();
 
-  let device = host
-    .default_output_device()
-    .expect("failed to find output device");
-  println!("Output device: {}", device.name().unwrap());
+  let mut n2 = NodeAdd::new();
 
-  let config = device.default_output_config().unwrap();
-  println!("Default output config: {:?}", config);
-
-  match config.sample_format() {
-    cpal::SampleFormat::F32 => run::<f32>(&device, &config.into()),
-    cpal::SampleFormat::I16 => run::<i16>(&device, &config.into()),
-    cpal::SampleFormat::U16 => run::<u16>(&device, &config.into()),
-  }
-}
-
-pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig)
-where
-  T: cpal::Sample,
-{
-  let sample_rate = config.sample_rate.0 as f32;
-  let channels = config.channels as usize;
-
-  // let arr = make_arr(sample_rate);
-  const LENGTH: usize = 20_000;
-  let mut arr = [0.0f32; LENGTH];
-
-  for (i, elem) in arr.iter_mut().enumerate() {
-    let sample_clock = (i + 1) % sample_rate as usize;
-    *elem = wave(waves::WaveType::Triangle, 80.0, sample_clock, sample_rate)
-  }
-
-  let mut file = File::create("js/src/data.data").expect("CANNOT CREATE");
-  file
-    .write(sample_rate.to_string().as_bytes())
-    .expect("ERROR WRITE");
-  file.write(b",").expect("ERROR WRITE");
-
-  arr.iter().for_each(|v| {
-    file.write(v.to_string().as_bytes()).expect("ERROR WRITE");
-    file.write(b",").expect("ERROR WRITE");
-  });
-
-  let mut i = 0;
-  let mut next_value = move || {
-    i = (i + 1) % LENGTH;
-    if i >= LENGTH {
-      println!("ERR LENGTH")
+  let a = n1._inputs.get_mut("a");
+  match a {
+    Some(v) => {
+      v.nodeRef = Some(NodeRef {
+        node: &n2,
+        output: "out",
+      })
     }
-    return arr[i];
-  };
-
-  let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-  let stream = device
-    .build_output_stream(
-      config,
-      move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-        write_data(data, channels, &mut next_value)
-      },
-      err_fn,
-    )
-    .unwrap();
-  stream.play().unwrap();
-
-  std::thread::sleep(std::time::Duration::from_millis(1000));
-}
-
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
-where
-  T: cpal::Sample,
-{
-  for frame in output.chunks_mut(channels) {
-    let value: T = cpal::Sample::from::<f32>(&next_sample());
-    for sample in frame.iter_mut() {
-      *sample = value;
-    }
+    None => (),
   }
+
+  match n2._inputs.get_mut("a") {
+    Some(v) => v.default = 5.0,
+    None => (),
+  }
+  match n2._inputs.get_mut("b") {
+    Some(v) => v.default = 10.0,
+    None => (),
+  }
+  match n1._inputs.get_mut("b") {
+    Some(v) => v.default = 10.0,
+    None => (),
+  }
+
+  n2.compute();
+
+  n1.compute();
+
+  let res = n1._outputs.get("out");
+  println!("{:?}", res)
 }
